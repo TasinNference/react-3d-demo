8278967cd60efa7aba61e728a1e13fc6fc6c7a09
Tasin Ahmed <tahmed@nference.net>
2022-09-03 18:11:53 -0400

    Gizmo color variables

--- src/components/ThreeCanvas/index.jsx
+++ src/components/ThreeCanvas/index.jsx
@@ -3,16 +3,14 @@ import { CanvasContainer } from "./styles";
 import { OrbitControls, GizmoHelper, GizmoViewport } from "@react-three/drei";
 import Slide from "../Slide";
 import CameraElement from "../CameraElement";
+import { AXIS_COLORS } from "../../constants/variables";
 
 const ThreeCanvas = () => {
   return (
     <CanvasContainer>
       <Canvas>
         <GizmoHelper>
-          <GizmoViewport
-            axisColors={["red", "green", "blue"]}
-            labelColor="white"
-          />
+          <GizmoViewport axisColors={AXIS_COLORS} labelColor="white" />
         </GizmoHelper>
         <Slide url="https://www.educationcorner.com/images/featured-improve-test-taking.jpg" />
         <CameraElement />
--- src/constants/variables.js
+++ src/constants/variables.js
@@ -3,3 +3,9 @@ export const ZOOM_LEVEL = 0.3;
 export const NEAR_VALUE = 1;
 export const FAR_VALUE = 4000;
 export const CAMERA_POSITION = [0, 0, 2000];
+
+// Gizmo Axis
+export const X_COLOR = "red";
+export const Y_COLOR = "green";
+export const Z_COLOR = "blue";
+export const AXIS_COLORS = [X_COLOR, Y_COLOR, Z_COLOR];
