3438be25077f1c40e2b98079090cd8105f3a1fbc
Tasin Ahmed <tahmed@nference.net>
2022-09-03 01:03:29 -0400

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ThreeCanvas from './ThreeCanvas'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <ThreeCanvas />
+    </div>
+  )
+}
+
+export default App
